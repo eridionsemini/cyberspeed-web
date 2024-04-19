0eb03ef20b99bf0a4b3858fbafe2845083be4f41
eridion semini <eridionsemini@gmail.com>
2024-04-19 21:09:29 +0200

    setup redux and sdk

---
+++ src/context/sdk.tsx
@@ -0,0 +1,19 @@
+import  {createContext, FC, ReactElement, ReactNode} from 'react';
+
+import {MoviesSDK} from 'movies-sdk';
+
+interface SDKContextProps {
+  children: ReactNode;
+}
+
+const {getActions, getSelectors, getStore} = new MoviesSDK();
+
+export const SDKContext = createContext({getActions, getSelectors, getStore});
+
+export const SDKContextProvider: FC<SDKContextProps> = ({children}): ReactElement => {
+  return (
+    <SDKContext.Provider value={{getActions, getSelectors, getStore}}>
+      {children}
+    </SDKContext.Provider>
+  );
+};
