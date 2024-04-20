85dcd015e119c388bc2ad4a1e9ce990993acd556
eridion semini <eridionsemini@gmail.com>
2024-04-20 13:40:32 +0200

    remove eslint warning

--- src/pages/movies.tsx
+++ src/pages/movies.tsx
@@ -1,19 +1,19 @@
-import {FC, ReactElement, useContext, useEffect, useState} from 'react';
+import { FC, ReactElement, useContext, useEffect, useState } from 'react';
 
-import {useNavigate} from 'react-router-dom';
-import {Movie as MovieType} from 'movies-sdk';
+import { useNavigate } from 'react-router-dom';
+import { Movie as MovieType } from 'movies-sdk';
 
-import {SDKContext} from '../context';
-import {useAppDispatch, useAppSelector} from '../hooks';
-import {chunkArray, debounce, isFavourite} from '../utils';
-import {Movie, Navbar} from '../components';
+import { SDKContext } from '../context';
+import { useAppDispatch, useAppSelector } from '../hooks';
+import { chunkArray, debounce, isFavourite } from '../utils';
+import { Movie, Navbar } from '../components';
 
 const Movies: FC = (): ReactElement => {
   const [fetched, setFetched] = useState<boolean>(false);
 
-  const {getActions, getSelectors, apiKey} = useContext(SDKContext);
+  const { getActions, getSelectors, apiKey } = useContext(SDKContext);
 
-  const {favouritesSelector, moviesSelector} = getSelectors();
+  const { favouritesSelector, moviesSelector } = getSelectors();
   const {
     getMoviesList,
     incrementMoviesListPage,
@@ -29,10 +29,10 @@ const Movies: FC = (): ReactElement => {
     loading,
     hasMore,
     page,
-    filter: {s},
+    filter: { s },
   } = useAppSelector(moviesSelector);
 
-  const {data: fav} = useAppSelector(favouritesSelector);
+  const { data: fav } = useAppSelector(favouritesSelector);
 
   const dispatch = useAppDispatch();
 
@@ -40,13 +40,13 @@ const Movies: FC = (): ReactElement => {
 
   const loadMoreClick = () => {
     if (hasMore) {
-      dispatch(loadMoreMovies({page: page + 1, s, apiKey: apiKey ? apiKey : ''}));
+      dispatch(loadMoreMovies({ page: page + 1, s, apiKey: apiKey ? apiKey : '' }));
       dispatch(incrementMoviesListPage());
     }
   };
 
   const handleClick = (v: string) => {
-    dispatch(getMovieDetails({i: v, apiKey: apiKey ? apiKey : ''}));
+    dispatch(getMovieDetails({ i: v, apiKey: apiKey ? apiKey : '' }));
     navigate(`/movie/${v}`);
   };
 
@@ -60,20 +60,20 @@ const Movies: FC = (): ReactElement => {
   };
 
   const handleDebouncedInputChange = debounce((value: string) => {
-    dispatch(getMoviesList({page, s: value, apiKey: apiKey ? apiKey : ''}));
+    dispatch(getMoviesList({ page, s: value, apiKey: apiKey ? apiKey : '' }));
   }, 1000);
 
   const handleInputChange = (txt: string) => {
-    dispatch(setFilterValue({key: 's', value: txt}));
+    dispatch(setFilterValue({ key: 's', value: txt }));
     handleDebouncedInputChange(txt);
   };
 
   useEffect(() => {
     if (!loading && data && data.length === 0 && !fetched) {
-      dispatch(getMoviesList({s, page: 1, apiKey: apiKey ? apiKey : ''}));
+      dispatch(getMoviesList({ s, page: 1, apiKey: apiKey ? apiKey : '' }));
       setFetched(true);
     }
-  }, [data, dispatch, loading, page, fetched, getMoviesList]);
+  }, [data, dispatch, loading, page, fetched, getMoviesList, apiKey, s]);
 
   const arrayMovies = chunkArray(data, 2);
 
